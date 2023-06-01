02554ee401bcfd35cbe547ffe3c928f2333a3e73
martin corpus <nasuntao@yahoo.com>
2023-06-01 18:18:36 +1200

    group project - commit database

---
+++ client/data/bones.ts
@@ -0,0 +1,53 @@
+const bonesData = [
+    {
+        id: 1,
+        phrase: 'In an ill-fated attempt at extreme hula-hooping, my enthusiasm took a painful turn when the hoop got out of control and caused a bone-breaking collision.',
+    }, 
+    {
+        id: 2,
+        phrase: 'I attempted to ride a unicycle down a steep hill, but my lack of balance led to a bone-breaking tumble.',
+    },
+    {
+        id: 3,
+        phrase: 'I decided to challenge a sumo wrestler to a match, but the outcome was a bone-breaking defeat rather than victory.',
+    },
+    {
+        id: 4,
+        phrase: 'In a misguided attempt at extreme yoga, I twisted my body into an improbable pose, resulting in a bone-breaking stretch.',
+    },
+    {
+        id: 5,
+        phrase: 'During an enthusiastic attempt at breakdancing, my body twisted in unimaginable ways, including a bone-breaking contortion.',
+    },
+    {
+        id: 6,
+        phrase: 'I decided to test my strength by arm-wrestling an actual gorilla, but it proved to be a bone-breaking decision on my part.',
+    },
+    {
+        id: 7,
+        phrase: 'I thought I could defy the laws of physics by attempting a flying leap off a trampoline, but my ambitious stunt resulted in a bone-breaking reality check.',
+    },
+    {
+        id: 8,
+        phrase: 'In a moment of extreme enthusiasm during a pillow fight, I swung my pillow with such vigor that it caused a bone-breaking impact.',
+    },
+    {
+        id: 9,
+        phrase: 'In an attempt to embrace my inner acrobat, I somersaulted off a swing and experienced a bone-breaking landing.',
+    },
+    {
+        id: 10,
+        phrase: 'During an intense game of limbo, I decided to impress everyone with my flexibility, only to end up with a bone-breaking limbo fail.',
+    },
+    {
+        id: 11,
+        phrase: 'I thought I could conquer the world of extreme pogo-sticking, but my enthusiasm led to an unfortunate bone-breaking misadventure.',
+    },
+    {
+        id: 12,
+        phrase: 'I thought it would be impressive to balance on a rolling office chair, but it quickly turned into a bone-breaking balancing act gone wrong.',
+    },
+    
+  ]
+  
+  export default bonesData
---
+++ client/data/linkingWord1.ts
@@ -0,0 +1,45 @@
+const linkingWord1 = [
+    {
+        id: 1,
+        phrase: 'Besides, ',
+    }, 
+    {
+        id: 2,
+        phrase: 'And then, ',
+    },
+    {
+        id: 3,
+        phrase: 'Furthermore, ',
+    },
+    {
+        id: 4,
+        phrase: 'Moreover, ',
+    },
+    {
+        id: 5,
+        phrase: 'Likewise, ',
+    },
+    {
+        id: 6,
+        phrase: 'Similarly, ',
+    },
+    {
+        id: 7,
+        phrase: 'Furthermore, ',
+    },
+    {
+        id: 8,
+        phrase: 'Also, ',
+    },
+    {
+        id: 9,
+        phrase: 'Additionally, ',
+    },
+    {
+        id: 10,
+        phrase: 'In addition, ',
+    },
+    
+  ]
+  
+  export default linkingWord1

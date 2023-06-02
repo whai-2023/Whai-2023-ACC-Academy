41a077410ef32af6ae872df9484f33b08e2b72e8
martin corpus <nasuntao@yahoo.com>
2023-06-02 13:51:22 +1200

    group project - trial and error

--- client/components/footer3.tsx
+++ client/components/footer3.tsx
@@ -1,16 +1,58 @@
+import { useState, useEffect } from 'react';
+import bloodData from '../data/blood.js';
+import bonesData from '../data/bones.js';
+import concussionData from '../data/concussion.js';
+
 interface Props {
-  injuries: null | string[]
+  injuries: null | string[];
 }
 
 export default function Footer3(props: Props) {
+  const [injuryContent, setInjuryContent] = useState<string>('');
+
+  useEffect(() => {
+    if (props.injuries !== null) {
+      const generateInjuryContent = () => {
+        let content = ''
+        for (let i = 0; i < props.injuries.length; i++) {
+          const injury = props.injuries[i]
+          const randomId = Math.floor(Math.random() * 12) + 1
+          const databaseContent = getDatabaseContent(injury, randomId.toString())
+          content += databaseContent
+        }
+        return content
+      }
+  
+      const getDatabaseContent = (injury: string, randomId: string) => {
+        let database
+        switch (injury) {
+          case 'blood':
+            database = bloodData
+            break;
+          case 'bones':
+            database = bonesData
+            break;
+          case 'concussion':
+            database = concussionData
+            break
+          default:
+            return ''
+        }
+        const selectedEntry = database.find((entry) => entry.id.toString() === randomId)
+        return selectedEntry ? selectedEntry.phrase : ''
+      };
+  
+      const content = generateInjuryContent()
+      setInjuryContent(content)
+    }
+  }, [props.injuries])
+
   return (
     <div className="footer-container">
       <div className="left-content">
         <div>
           <div className="speech-balloon">
-            <span className="text">
-              What is your injury? {JSON.stringify(props)}
-            </span>
+            <span className="text">What is your injury? {injuryContent}</span>
           </div>
         </div>
       </div>
@@ -22,5 +64,5 @@ export default function Footer3(props: Props) {
         />
       </div>
     </div>
-  )
+  );
 }

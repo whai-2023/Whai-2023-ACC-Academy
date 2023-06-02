82f2352b1b8abcf7f802c0eefeec59b877f3452e
martin corpus <nasuntao@yahoo.com>
2023-06-02 14:51:22 +1200

    group project - trial

--- client/components/footer3.tsx
+++ client/components/footer3.tsx
@@ -1,58 +1,23 @@
-import { useState, useEffect } from 'react';
-import bloodData from '../data/blood.js';
-import bonesData from '../data/bones.js';
-import concussionData from '../data/concussion.js';
+import { useState, useEffect } from 'react'
+
+
 
 interface Props {
-  injuries: null | string[];
+  injuries: null | string[]
+  injuryContent: string
 }
 
 export default function Footer3(props: Props) {
-  const [injuryContent, setInjuryContent] = useState<string>('');
 
-  useEffect(() => {
-    if (props.injuries !== null) {
-      const generateInjuryContent = () => {
-        let content = ''
-        for (let i = 0; i < props.injuries.length; i++) {
-          const injury = props.injuries[i]
-          const randomId = Math.floor(Math.random() * 12) + 1
-          const databaseContent = getDatabaseContent(injury, randomId.toString())
-          content += databaseContent
-        }
-        return content
-      }
-  
-      const getDatabaseContent = (injury: string, randomId: string) => {
-        let database
-        switch (injury) {
-          case 'blood':
-            database = bloodData
-            break;
-          case 'bones':
-            database = bonesData
-            break;
-          case 'concussion':
-            database = concussionData
-            break
-          default:
-            return ''
-        }
-        const selectedEntry = database.find((entry) => entry.id.toString() === randomId)
-        return selectedEntry ? selectedEntry.phrase : ''
-      };
-  
-      const content = generateInjuryContent()
-      setInjuryContent(content)
-    }
-  }, [props.injuries])
+
+ 
 
   return (
     <div className="footer-container">
       <div className="left-content">
         <div>
           <div className="speech-balloon">
-            <span className="text">What is your injury? {injuryContent}</span>
+            <span className="text">What is your injury? {props.injuryContent}</span>
           </div>
         </div>
       </div>
--- client/components/questionnaire.tsx
+++ client/components/questionnaire.tsx
@@ -2,13 +2,26 @@ import Header from './header'
 import Body3 from './body3'
 import Footer3 from './footer3'
 import { useState } from 'react'
+import bloodData from '../data/blood.js'
 
 function Questionnaire() {
   const [injuries, setInjuries] = useState(null as null | string[])
 
   function handleInjuriesChange(injuries: null | string[]) {
     setInjuries(injuries)
+    injuryOutput()
   }
+
+  const [injuryContent, setInjuryContent] = useState<string>('');
+
+  
+  function injuryOutput() {
+    const database = bloodData
+    const randomId = Math.floor(Math.random() * 12) + 1
+    const dbentry = database.find((entry) => entry.id === randomId)
+
+    setInjuryContent(dbentry?.phrase as string)
+  } 
 
   return (
     <>
@@ -19,7 +32,7 @@ function Questionnaire() {
         <Body3 onSubmit={handleInjuriesChange} />
       </div>
       <div className="footer2">
-        <Footer3 injuries={injuries} />
+        <Footer3 injuries={injuries} injuryContent={injuryContent} />
       </div>
     </>
   )
